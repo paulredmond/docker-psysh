f9f86afce81649333bf6a141af8e35c665483cda
Paul Redmond <paulrredmond@gmail.com>
2019-11-29 15:58:58 -0700

    WIP

--- bin/test.sh
+++ bin/test.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
 docker build --pull -t paulredmond/psysh:7.4 -f 7.4/Dockerfile 7.4/
-GOSS_FILES_PATH="$(pwd)/7.4" dgoss run -i paulredmond/psysh:7.4
+GOSS_PATH="$(pwd)/goss" \
+GOSS_FILES_PATH="$(pwd)/7.4" \
+./dgoss run -i paulredmond/psysh:7.4
